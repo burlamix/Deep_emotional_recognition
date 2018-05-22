83814586858e41d5fba7d45775f698b1394d3b34
burlamix <burlami@gmail.com>
2018-05-22 16:03:36 +0200

    test server

--- FFNN.py
+++ FFNN.py
@@ -17,19 +17,19 @@ trainable = 'True'
 numpy.random.seed(6)
 
 #emotions = ['ang','dis','exc','fea','fru','hap','neu','oth','sad','sur','xxx']
-emotions = ['sad','hap']#,'ang','exc']
+emotions = ['ang','exc']#,'ang','exc']
 size_batch = 32
 frame_number = 20
 
 
 model = Sequential()
-model.add(Dense(254, activation='relu', input_dim=frame_number*33, name='dense_1',kernel_initializer='VarianceScaling'))
+model.add(Dense(256, activation='relu', input_dim=frame_number*33, name='dense_1',kernel_initializer='VarianceScaling'))
 #model.add(Dropout(0.5))
-model.add(Dense(254, activation='relu', name='dense_2',kernel_initializer='VarianceScaling'))
+model.add(Dense(256, activation='relu', name='dense_2',kernel_initializer='VarianceScaling'))
 #model.add(Dropout(0.5))
-model.add(Dense(254, activation='relu', name='dense_3',kernel_initializer='VarianceScaling'))
+model.add(Dense(256, activation='relu', name='dense_3',kernel_initializer='VarianceScaling'))
 #model.add(Dropout(0.5))
-model.add(Dense(254, activation='relu', name='dense_4',kernel_initializer='VarianceScaling'))
+model.add(Dense(256, activation='relu', name='dense_4',kernel_initializer='VarianceScaling'))
 #model.add(Dropout(0.5))
 #model.add(Dense(100, activation='sigmoid', trainable=trainable,name='dense_5'))
 #model.add(Dense(64, activation='sigmoid', trainable=trainable,name='dense_6'))
@@ -37,12 +37,12 @@ model.add(Dense(254, activation='relu', name='dense_4',kernel_initializer='Varia
 #model.add(Dense(16, activation='sigmoid', trainable=trainable,name='dense_8'))
 #model.add(Dense(8, activation='sigmoid', trainable=trainable,name='dense_9'))
 #model.add(Dropout(0.5))
-model.add(Dense(len(emotions), activation='softmax',name='dense_55j'))
+model.add(Dense(len(emotions), activation='softmax',name='dense_f'))
 
 
 #some possible optimizer
 
-adam =keras.optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+adam =keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 sgd = SGD(lr=0.000000001, decay=1e-6, momentum=0.9, nesterov=True)
 
 #lr=0.0000001
@@ -50,9 +50,10 @@ model.compile(loss='categorical_crossentropy',
               optimizer=adam,
               metrics=['accuracy'])
 
-train_size,_,_ = total_number('train','M',emotions,size_batch,frame_number)
-validation_size,_,_ = total_number('validation','M',emotions,size_batch,frame_number)
-test_size,_,_ = total_number('test','M',emotions,size_batch,frame_number)
+train_size,_,_ = 		total_number('train','M',emotions,size_batch,frame_number)
+validation_size,_,_ = 	total_number('validation','M',emotions,size_batch,frame_number)
+test_size,_,_ = 		total_number('train','M',emotions,size_batch,frame_number)
+
 
 print("\nsize of train "+str(train_size)+"\n")
 print("size of test_size "+str(test_size)+"\n")
@@ -61,20 +62,22 @@ print("size of test_size "+str(test_size)+"\n")
 train_generator = dataset_generator(size_batch,'train','M',emotions,frame_number)
 validation_generator = dataset_generator(size_batch,'validation','M',emotions,frame_number)
 test_generator = dataset_generator(size_batch,'test','M',emotions,frame_number)
-train_generator_overfit = dataset_generator(size_batch,'test','M',emotions,frame_number)
 
 class_weight_dict = weight_class('train',emotions,'M')
 
-print(class_weight_dict)
+#print(class_weight_dict)
 
-model.fit_generator(train_generator, steps_per_epoch=train_size, epochs=300,shuffle=True,class_weight=class_weight_dict)
-
-pred = model.predict_generator( train_generator_overfit, steps=test_size)
-
+pred = model.predict_generator( validation_generator, steps=validation_size)
 print(numpy.sum(pred > 1/len(emotions),axis=0))
+print(model.evaluate_generator( validation_generator, steps=validation_size))
 
 
-print(model.evaluate_generator( train_generator_overfit, steps=test_size))
+model.fit_generator(train_generator, steps_per_epoch=train_size, epochs=1500,shuffle=True,class_weight=class_weight_dict)
+
+
+pred = model.predict_generator( validation_generator, steps=validation_size)
+print(numpy.sum(pred > 1/len(emotions),axis=0))
+print(model.evaluate_generator( validation_generator, steps=validation_size))
 
 
 #model.load_weights('weights',by_name=False)
