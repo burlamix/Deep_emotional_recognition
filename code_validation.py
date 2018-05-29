41743d3700bc39b34b945e91697778b904d37e41
oykukapcak <oykukapcak@sabanciuniv.edu>
2018-05-29 09:36:55 +0200

    number of nneuron combinations

--- code_validation.py
+++ code_validation.py
@@ -22,7 +22,7 @@ trainable = 'True'
 #emotions = ['ang','dis','exc','fea','fru','hap','neu','oth','sad','sur','xxx']
 emotions = ['hap','sad','ang','exc']
 size_batch2 = 32
-frame_number = 50
+frame_number = 100
 
 x,y,class_weight_dict = static_dataset('train','M',emotions,frame_number)
 x_test,y_test,class_weight_dict_test = static_dataset('test','M',emotions,frame_number)
@@ -31,70 +31,78 @@ x_test,y_test,class_weight_dict_test = static_dataset('test','M',emotions,frame_
 x = tf.keras.utils.normalize(x,    axis=-1,    order=2)
 
 #search parameters for hyperparameter optimization
-batch_sizes = [16, 32, 64, 128, 256]
-epochs = [10, 50, 100]
+#batch_sizes = [16, 32, 64, 128, 256]
+batch_sizes = [32]
+#epochs = [10, 50, 100]
+epochs = [500]
 #optimizers = [sgd, rmsdrop, adagrad, adadelta, adam, adamax, nadam]
-learn_rates = [0.00001, 0.0001, 0.001, 0.01, 0.1, 0.2]
-activations = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
-dropout_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-hidden1_neurons = [16, 32, 64, 128, 256]
-hidden2_neurons = [16, 32, 64, 128, 256]
-hidden3_neurons = [16, 32, 64, 128, 256]
-hidden4_neurons = [16, 32, 64, 128, 256]
+#learn_rates = [0.00001, 0.0001, 0.001, 0.01, 0.1, 0.2]
+learn_rates = [1e-5]
+#activations = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
+activations = ['sigmoid']
+#dropout_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+dropout_rates = [0.5]
+hidden_neurons = [[16,32,16,128], [32,32,32,128], [64,64,128,128]]
+#hidden1_neurons = [16, 32, 64, 128, 256]
+#hidden2_neurons = [16, 32, 64, 128, 256]
+#hidden3_neurons = [16, 32, 64, 128, 256]
+#hidden4_neurons = [16, 32, 64, 128, 256]
 
 
 for activation in activations:
 	for dropout in dropout_rates:
-		for hidden1 in hidden1_neurons:
-			for hidden2 in hidden2_neurons:
-				for hidden3 in hidden3_neurons:
-					for hidden4 in hidden4_neurons:
-						model = Sequential()
-						model.add(Dense(256, activation='relu', input_dim=frame_number*33, name='dense_1',kernel_initializer='glorot_normal'))
-						model.add(Dropout(0.5))
-						model.add(Dense(128, activation='relu', name='dense_2',kernel_initializer='glorot_normal'))
-						model.add(Dropout(0.5))
-						model.add(Dense(64, activation='relu', name='dense_3',kernel_initializer='glorot_normal'))
-						model.add(Dropout(0.5))
-						model.add(Dense(32, activation='relu', name='dense_4',kernel_initializer='glorot_normal'))
+		for hidden in hidden_neurons:
+			model = Sequential()
+			model.add(Dense(hidden[0], activation=activation, input_dim=frame_number*33, name='dense_1',kernel_initializer='glorot_uniform'))
+			model.add(Dropout(dropout))
+			model.add(Dense(hidden[1], activation=activation, name='dense_2',kernel_initializer='glorot_uniform'))
+			model.add(Dropout(dropout))
+			model.add(Dense(hidden[2], activation=activation, name='dense_3',kernel_initializer='glorot_uniform'))
+			model.add(Dropout(dropout))
+			model.add(Dense(hidden[3], activation=activation, name='dense_4',kernel_initializer='glorot_uniform'))
 
-						model.add(Dense(len(emotions), activation='softmax',name='dense_f'))
+			model.add(Dense(len(emotions), activation='softmax',name='dense_f'))
 
-						for learn_rate in learn_rates:
-							sgd = SGD(lr=learn_rate, decay=1e-6, momentum=0.9, nesterov=False)
-							rmsdrop = keras.optimizers.RMSprop(lr=learn_rate, rho=0.9, epsilon=None, decay=0.0)
-							adagrad = keras.optimizers.Adagrad(lr=learn_rate, epsilon=None, decay=0.0)
-							adadelta = keras.optimizers.Adadelta(lr=learn_rate, rho=0.95, epsilon=None, decay=0.0)
-							adam =keras.optimizers.Adam(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-							adamax = keras.optimizers.Adamax(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
-							nadam = keras.optimizers.Nadam(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
+			for learn_rate in learn_rates:
+				sgd = SGD(lr=learn_rate, decay=1e-6, momentum=0.9, nesterov=False)
+				rmsdrop = keras.optimizers.RMSprop(lr=learn_rate, rho=0.9, epsilon=None, decay=0.0)
+				adagrad = keras.optimizers.Adagrad(lr=learn_rate, epsilon=None, decay=0.0)
+				adadelta = keras.optimizers.Adadelta(lr=learn_rate, rho=0.95, epsilon=None, decay=0.0)
+				adam =keras.optimizers.Adam(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+				adamax = keras.optimizers.Adamax(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
+				nadam = keras.optimizers.Nadam(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
 
-							optimizers = [sgd, rmsdrop, adagrad, adadelta, adam, adamax, nadam]
+				#optimizers = [sgd, rmsdrop, adagrad, adadelta, adam, adamax, nadam]
+				optimizers = [adam]
 
-							#optimizer
-							#adam =keras.optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-							#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+				#optimizer
+				#adam =keras.optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+				#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-							for batchSize in batch_sizes:
-								for epoch in epochs:
-									for optimizer in optimizers:
+				for batchSize in batch_sizes:
+					for epoch in epochs:
+						for optimizer in optimizers:
 
-									model.compile(loss='categorical_crossentropy',
-									              optimizer=adam,
-									              metrics=['accuracy'])
+						model.compile(loss='categorical_crossentropy',
+						              optimizer=optimizer,
+						              metrics=['accuracy'])
 
-									with open("code_validation_results.txt", 'a') as outputFile:
-										outputFile.write("\n   ---training---")
-										outputFile.write(numpy.sum(model.predict(x=x,batch_size=1)> 1/len(emotions),axis=0))
+						with open("code_validation_results.txt", 'a') as outputFile:
+							outputFile.write("activation= %s, dropout= %f, optimizer= %s, batch size = %d, epoch= %d, learning rate= %f" % (activation, dropout, optimizer, batchSize, epoch, learn_rate))
+							outputFile.write("number of hidden1 neurons: %d, number of hidden2 neurons: %d, number of hidden3 neurons: %d, number of hidden4 neurons: %d" % (hidden1, hidden2, hidden3, hidden4))
+							#model.save_weights("weights")
+							outputFile.write("\n   ---training---")
+							outputFile.write(numpy.sum(model.predict(x=x,batch_size=1)> 1/len(emotions),axis=0))
 
 
-									model.fit(x=x,y=y,batch_size=size_batch2, epochs=300,shuffle=True,class_weight=class_weight_dict)
+						model.fit(x=x,y=y,batch_size=batchSize, epochs=epoch,shuffle=True,class_weight=class_weight_dict)
 
-									with open("code_validation_results.txt", 'a') as outputFile:
-										outputFile.write("\n   ---training---")
-										outputFile.write(numpy.sum(model.predict(x=x,batch_size=1)> 1/len(emotions),axis=0))
+						with open("code_validation_results.txt", 'a') as outputFile:
+							outputFile.write("\n   ---training---")
+							outputFile.write(numpy.sum(model.predict(x=x,batch_size=1)> 1/len(emotions),axis=0))
 
-										outputFile.write("\n   ---test---  ")
-										outputFile.write(numpy.sum(model.predict(x=x_test,batch_size=1)> 1/len(emotions),axis=0))
-										outputFile.write(model.evaluate(x=x_test,y=y_test,batch_size=1))
+							outputFile.write("\n   ---test---  ")
+							outputFile.write(numpy.sum(model.predict(x=x_test,batch_size=1)> 1/len(emotions),axis=0))
+							outputFile.write(model.evaluate(x=x_test,y=y_test,batch_size=1))
+
 
