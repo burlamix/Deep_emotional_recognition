dccb971408b1636f3305317bd120d905a9c87328
burlamix <burlami@gmail.com>
2018-05-29 11:11:13 +0200

    simple network

--- FFNN.py
+++ FFNN.py
@@ -24,20 +24,20 @@ trainable = 'True'
  #numpy.random.seed(6)
 
 #emotions = ['ang','dis','exc','fea','fru','hap','neu','oth','sad','sur','xxx']
-emotions = ['hap','sad','ang','exc']
+emotions = ['hap','sad']#,'ang','exc']
 size_batch2 = 32
 frame_number = 50
 
 
 model = Sequential()
-model.add(Dense(256, activation='relu', input_dim=frame_number*33, name='dense_1',kernel_initializer='glorot_uniform'))
+model.add(Dense(200, activation='relu', input_dim=frame_number*33, name='dense_1',kernel_initializer='glorot_uniform'))
 #model.add(BatchNormalization())
 model.add(Dropout(0.5))
-model.add(Dense(128, activation='relu', name='dense_2',kernel_initializer='glorot_uniform'))
+model.add(Dense(100, activation='relu', name='dense_2',kernel_initializer='glorot_uniform'))
 model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu', name='dense_3',kernel_initializer='glorot_uniform'))
+model.add(Dense(50, activation='relu', name='dense_3',kernel_initializer='glorot_uniform'))
 model.add(Dropout(0.5))
-model.add(Dense(32, activation='relu', name='dense_4',kernel_initializer='glorot_uniform'))
+model.add(Dense(25, activation='relu', name='dense_4',kernel_initializer='glorot_uniform'))
 #model.add(Dropout(0.5))
 #model.add(Dense(100, activation='sigmoid', trainable=trainable,name='dense_5'))
 #model.add(Dense(64, activation='sigmoid', trainable=trainable,name='dense_6'))
